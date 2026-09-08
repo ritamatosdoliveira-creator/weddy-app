dd76462d9d442c93fcbeb4567ed00fed992315ee
ritamatosdoliveira-creator <ritamatosdoliveira@gmail.com>
2026-09-08 12:18:03 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -10,10 +10,11 @@ self.addEventListener('activate', (event) => {
 });
 self.addEventListener('fetch', (event) => {
   if (event.request.method !== 'GET') return;
+  if (!event.request.url.startsWith('http')) return;
   event.respondWith(
     caches.match(event.request).then((cached) => cached || fetch(event.request).then((response) => {
       const copy = response.clone();
-      caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+      caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy)).catch(()=>{});
       return response;
     }).catch(() => cached))
   );
