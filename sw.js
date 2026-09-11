ce4bfc7c4bc378153498a095d42d269bd4c29ff6
ritamatosdoliveira-creator <ritamatosdoliveira@gmail.com>
2026-09-11 09:49:14 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'weddy-v11';
+const CACHE_NAME = 'weddy-v13';
 const FILES_TO_CACHE = ['./','./index.html','./manifest.json','./icon-180.png','./icon-192.png','./icon-512.png','./login-bg.jpg'];
 self.addEventListener('install', (event) => {
   event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE)));
